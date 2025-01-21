4e2b9be9cf45995bbd4d77cc3eddd3c9bf34caa9
Adriano-Cardoso <goularta97@gmail.com>
2025-01-21 15:09:11 -0300

    ""

--- vars/testEchoServer.groovy
+++ vars/testEchoServer.groovy
@@ -1,30 +1,18 @@
-def testEchoServer(String message) {
+def call(String message) {
     echo "Testing with message: ${message}"
 
-    // Envia a mensagem ao Echo Server usando curl
+    // Envia a mensagem para o Echo Server
     def response = sh(
-        script: "curl -X POST -d '${message}' -H 'Content-Type: text/plain' http://localhost:8081",
+        script: "echo '${message}' | nc localhost 8081",
         returnStdout: true
     ).trim()
 
-    echo "Raw response from Echo Server: ${response}"
+    echo "Response from Echo Server: ${response}"
 
-    // Verifique se a resposta contém o corpo esperado
-    def responseBody = null
-    def match = (response =~ /(?:\r?\n){2}(.*)/)
-    
-    if (match) {
-        responseBody = match[0][1]?.trim()
-        echo "Extracted Response Body: ${responseBody}"
-    } else {
-        error "Unexpected response format, no body extracted"
-    }
-
-    // Verifique se o corpo da resposta contém a mensagem esperada
-    if (responseBody == "Message Received: ${message}") {
+    // Valida a resposta do Echo Server
+    if (response == "Message Received: ${message}") {
         echo "Message received correctly by the Echo Server!"
     } else {
-        // Falha no pipeline caso a mensagem não tenha sido recebida como esperado
         error "Message was not received correctly by the Echo Server!"
     }
 }
