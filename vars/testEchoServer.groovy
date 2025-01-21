f425b31acde570534faeb6570457410d48725cda
Adriano-Cardoso <goularta97@gmail.com>
2025-01-21 15:14:37 -0300

    feat: new method

--- vars/testEchoServer.groovy
+++ vars/testEchoServer.groovy
@@ -1,16 +1,19 @@
 def call(String message) {
     echo "Testing with message: ${message}"
 
-    // Envia a mensagem para o Echo Server
+    // Envia a mensagem para o Echo Server via curl
     def response = sh(
-        script: "echo '${message}' | nc localhost 8081",
+        script: "curl -X POST -d '${message}' http://localhost:8081",
         returnStdout: true
     ).trim()
 
     echo "Response from Echo Server: ${response}"
 
+    // Extrai a parte relevante da resposta (assumindo que o Echo Server responde com "Message Received: <mensagem>")
+    def messageReceived = response.split("\n").last().trim()
+
     // Valida a resposta do Echo Server
-    if (response == "Message Received: ${message}") {
+    if (messageReceived == "Message Received: ${message}") {
         echo "Message received correctly by the Echo Server!"
     } else {
         error "Message was not received correctly by the Echo Server!"
