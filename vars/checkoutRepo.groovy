485c72faf8110bf97a2beb1676c55f32f583ef6c
Adriano-Cardoso <goularta97@gmail.com>
2025-01-23 10:25:28 -0300

    new commit in library

--- vars/checkoutRepo.groovy
+++ vars/checkoutRepo.groovy
@@ -1,6 +1,12 @@
 def call(String repoUrl, String branch, String credentialsId) {
-    echo "Starting repository checkout"
+  echo "Starting repository checkout"
+  try {
     git credentialsId: credentialsId,
-        url: repoUrl,
-        branch: branch
+      url: repoUrl,
+      branch: branch
+    echo 'Repository checked out successfully!'
+  } catch (err) {
+    echo "Error checking out repository: ${err.message}"
+    // You can also notify the user or perform other actions upon error
+  }
 }
--- vars/testEchoServer.groovy
+++ vars/testEchoServer.groovy
@@ -1,7 +1,7 @@
 def call(String message) {
     echo "Testing with message: ${message}"
 
-    // Envia a mensagem para o Echo Server via curl
+    // Send the message to the Echo Server via curl
     def response = sh(
         script: "curl -X POST -d '${message}' http://localhost:8081",
         returnStdout: true
@@ -9,10 +9,9 @@ def call(String message) {
 
     echo "Response from Echo Server: ${response}"
 
-    // Extrai a parte relevante da resposta (assumindo que o Echo Server responde com "Message Received: <mensagem>")
+    // Extract the relevant part of the response (assuming the Echo Server responds with "Message Received: <message>")
     def messageReceived = response.split("\n").last().trim()
 
-    // Valida a resposta do Echo Server
     if (messageReceived == "Message Received: ${message}") {
         echo "Message received correctly by the Echo Server!"
     } else {
